da259b892ef214642ddab0dbc5cb66db22865379
RiteshHedau <riteshhedau7@gmail.com>
2025-04-10 08:33:27 +0530

    commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -184,7 +184,7 @@ const Navigation = () => {
             <Link to="/" className="flex items-center space-x-3">
               <FaGraduationCap className="h-8 w-8 text-blue-600" />
               <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
-                LearnCraft
+              TechLearn
               </span>
             </Link>
           </div>
